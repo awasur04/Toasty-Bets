c31e3d0b58a6a0fb03ea4989fbfe70ab5f707555
awasur04 <awasur04@yahoo.com>
2021-09-27 22:26:58 -0500

    Add coming soon status
    Added a coming soon discord status

--- src/main/java/com/github/awasur04/toastybets/ToastyBets.java
+++ src/main/java/com/github/awasur04/toastybets/ToastyBets.java
@@ -2,10 +2,13 @@ package com.github.awasur04.toastybets;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.presence.Activity;
+import discord4j.core.object.presence.Presence;
 
 public class ToastyBets {
     public static void main(String[] args) {
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build().login().block();
         client.onDisconnect().block();
+        client.updatePresence(Presence.online(Activity.playing("Coming Soon ;)")));
     }
 }
