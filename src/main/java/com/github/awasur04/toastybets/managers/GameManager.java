57c14c1e128c59cba46d2f81ede4eecfb59bd084
awasur04 <awasur04@yahoo.com>
2021-10-04 16:08:22 -0500

    Added abbreviations for team list
    Added each teams abbreviation for team list

--- src/main/java/com/github/awasur04/toastybets/managers/GameManager.java
+++ src/main/java/com/github/awasur04/toastybets/managers/GameManager.java
@@ -2,7 +2,7 @@ package com.github.awasur04.toastybets.managers;
 
 import com.github.awasur04.toastybets.models.Game;
 import com.github.awasur04.toastybets.models.Team;
-import com.github.awasur04.toastybets.services.ScheduleService;
+import com.github.awasur04.toastybets.services.UpdateGames;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -11,44 +11,45 @@ import java.util.Map;
 
 public class GameManager {
     private Map<Integer, Team> teamList;
-    private ArrayList<Game> currentWeekGames;
-    private ScheduleService scheduleService;
+    private HashMap<Long, Game> currentWeekGames;
+    private UpdateGames update;
+    private ScheduledEventsManager eventsManager;
 
 
     public GameManager() {
         this.teamList = new HashMap<>() {{
-            put(1, new Team(1,"Atlanta Falcons"));
-            put(2, new Team(2, "Buffalo Bills"));
-            put(3, new Team(3, "Chicago Bears"));
-            put(4, new Team(4, "Cincinnati Bengals"));
-            put(5, new Team(5, "Cleveland Browns"));
-            put(6, new Team(6, "Dallas Cowboys"));
-            put(7, new Team(7, "Denver Broncos"));
-            put(8, new Team(8, "Detroit Lions"));
-            put(9, new Team(9, "Green Bay Packers"));
-            put(10, new Team(10, "Tennessee Titans"));
-            put(11, new Team(11, "Indianapolis Colts"));
-            put(12, new Team(12, "Kansas City Chiefs"));
-            put(13, new Team(13, "Las Vegas Raiders"));
-            put(14, new Team(14, "Las Angeles Rams"));
-            put(15, new Team(15, "Miami Dolphins"));
-            put(16, new Team(16, "Minnesota Vikings"));
-            put(17, new Team(17, "New England Patriots"));
-            put(18, new Team(18, "New Orleans Saints"));
-            put(19, new Team(19, "New York Giants"));
-            put(20, new Team(20, "New York Jets"));
-            put(21, new Team(21, "Philadelphia Eagles"));
-            put(22, new Team(22, "Arizona Cardinals"));
-            put(23, new Team(23, "Pittsburgh Steelers"));
-            put(24, new Team(24, "Los Angeles Chargers"));
-            put(25, new Team(25, "San Francisco 49ers"));
-            put(26, new Team(26, "Seattle Seahawks"));
-            put(27, new Team(27, "Tampa Bay Buccaneers"));
-            put(28, new Team(28, "Washington Football Team"));
-            put(29, new Team(29, "Carolina Panthers"));
-            put(30, new Team(30, "Jacksonville Jaguars"));
-            put(33, new Team(33, "Baltimore Ravens"));
-            put(34, new Team(34, "Houston Texans"));
+            put(1, new Team(1,"Atlanta Falcons", "ATL"));
+            put(2, new Team(2, "Buffalo Bills", "BUF"));
+            put(3, new Team(3, "Chicago Bears", "CHI"));
+            put(4, new Team(4, "Cincinnati Bengals", "CIN"));
+            put(5, new Team(5, "Cleveland Browns", "CLE"));
+            put(6, new Team(6, "Dallas Cowboys", "DAL"));
+            put(7, new Team(7, "Denver Broncos", "DEN"));
+            put(8, new Team(8, "Detroit Lions", "DET"));
+            put(9, new Team(9, "Green Bay Packers", "GB"));
+            put(10, new Team(10, "Tennessee Titans", "TEN"));
+            put(11, new Team(11, "Indianapolis Colts", "IND"));
+            put(12, new Team(12, "Kansas City Chiefs", "KC"));
+            put(13, new Team(13, "Las Vegas Raiders", "LV"));
+            put(14, new Team(14, "Las Angeles Rams", "LAR"));
+            put(15, new Team(15, "Miami Dolphins", "MIA"));
+            put(16, new Team(16, "Minnesota Vikings", "MIN"));
+            put(17, new Team(17, "New England Patriots", "NE"));
+            put(18, new Team(18, "New Orleans Saints", "NO"));
+            put(19, new Team(19, "New York Giants", "NYG"));
+            put(20, new Team(20, "New York Jets", "NYJ"));
+            put(21, new Team(21, "Philadelphia Eagles", "PHI"));
+            put(22, new Team(22, "Arizona Cardinals", "ARI"));
+            put(23, new Team(23, "Pittsburgh Steelers", "PIT"));
+            put(24, new Team(24, "Los Angeles Chargers", "LAC"));
+            put(25, new Team(25, "San Francisco 49ers", "SF"));
+            put(26, new Team(26, "Seattle Seahawks", "SEA"));
+            put(27, new Team(27, "Tampa Bay Buccaneers", "TB"));
+            put(28, new Team(28, "Washington Football Team", "WAS"));
+            put(29, new Team(29, "Carolina Panthers", "CAR"));
+            put(30, new Team(30, "Jacksonville Jaguars", "JAX"));
+            put(33, new Team(33, "Baltimore Ravens", "BAL"));
+            put(34, new Team(34, "Houston Texans", "HOU"));
         }};
         this.currentWeekGames = new ArrayList<>();
         this.scheduleService = new ScheduleService(this);
