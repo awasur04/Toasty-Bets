37cc1c617b5f3154b3458aa485f56da71e60f71f
awasur04 <awasur04@yahoo.com>
2021-10-04 16:05:05 -0500

    Added safety shutdown hook
    Added shutdown hook to close the execution service before stopping program

--- src/main/java/com/github/awasur04/toastybets/ToastyBets.java
+++ src/main/java/com/github/awasur04/toastybets/ToastyBets.java
@@ -1,5 +1,6 @@
 package com.github.awasur04.toastybets;
 
+import com.github.awasur04.toastybets.managers.GameManager;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.presence.Activity;
@@ -7,6 +8,9 @@ import discord4j.core.object.presence.Presence;
 
 public class ToastyBets {
     public static void main(String[] args) {
+        GameManager gm = new GameManager();
+        Runtime.getRuntime().addShutdownHook(new Thread(gm::closeProgram));
+
         final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build().login().block();
         client.onDisconnect().block();
         client.updatePresence(Presence.online(Activity.playing("Coming Soon ;)")));
