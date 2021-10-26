b0d4b2bc3cd56f62e2960397b2f31eafce9c988d
awasur04 <awasur04@yahoo.com>
2021-10-26 14:57:53 -0500

    Added Log Support
    Created a log class to create logs and easily log errors and information to log file

---
+++ src/main/java/com/github/awasur04/toastybets/managers/LogManager.java
@@ -0,0 +1,58 @@
+package com.github.awasur04.toastybets.managers;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public final class LogManager {
+    static File logFile;
+    static BufferedWriter buffWriter;
+
+    public LogManager() {}
+
+    public static void logMessage(String message) {
+        try {
+            LocalDateTime currentDateAndTime = LocalDateTime.now();
+            String logFileName = "logs-" + currentDateAndTime.getYear() + "-" + currentDateAndTime.getMonthValue() + "-" + currentDateAndTime.getDayOfMonth() + ".txt";
+
+            logFile = new File("logs\\" + logFileName);
+            if (logFile.getParentFile().mkdir()) {
+                logFile.createNewFile();
+            }
+
+            buffWriter = new BufferedWriter(new FileWriter(logFile, true));
+            if (!message.isBlank()) {
+                String newMessage = "\n[" + currentDateAndTime.getYear() + "-" + currentDateAndTime.getMonthValue() + "-" + currentDateAndTime.getDayOfMonth() +
+                        "|" + currentDateAndTime.getHour() + ":" + currentDateAndTime.getMinute() + ":" + currentDateAndTime.getSecond() + "] " + message;
+                buffWriter.write(newMessage);
+                buffWriter.flush();
+            }
+        }catch (Exception e) {
+            System.out.println("Error: Log file could not be created\n" + e.getMessage());
+        }
+    }
+
+    public static void closeLogs() {
+        try {
+            buffWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error: Log System has not been created!");
+        } catch (Exception e) {
+            System.out.println("Error: Log system failed to close");
+        }
+    }
+
+    public static void log(String message) {
+        logMessage("LOG: " + message);
+    }
+
+    public static void warning(String message) {
+        logMessage("WARNING: " + message);
+    }
+
+    public static void error(String message, String error) {
+       logMessage("ERROR: " + message + " " + error);
+    }
+}
