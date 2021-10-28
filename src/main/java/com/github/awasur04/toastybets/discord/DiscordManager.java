653186fbfcec0388b630d3b7f778a21f1006e47e
awasur04 <awasur04@yahoo.com>
2021-10-28 02:10:54 -0500

    added Handlers and time zone support
    Added time zone map and added support for register command

--- src/main/java/com/github/awasur04/toastybets/discord/DiscordManager.java
+++ src/main/java/com/github/awasur04/toastybets/discord/DiscordManager.java
@@ -1,35 +1,57 @@
 package com.github.awasur04.toastybets.discord;
 
 import com.github.awasur04.toastybets.database.DatabaseManager;
+import com.github.awasur04.toastybets.managers.GameManager;
 import com.github.awasur04.toastybets.managers.LogManager;
+import com.github.awasur04.toastybets.models.User;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
-import javax.security.auth.login.LoginException;
+import java.time.ZoneId;
+import java.util.HashMap;
+
+import static net.dv8tion.jda.api.utils.data.DataType.STRING;
 
 public class DiscordManager {
     private String token;
+    private GameManager gameManager;
     private DatabaseManager databaseManager;
     private JDA toastyBot;
+    private ResponseHandler responseHandler;
+
+    private static HashMap<String, String> timeZoneUTC = new HashMap<>() {{
+        put("EST", "UTC-4");
+        put("CST", "UTC-5");
+        put("MST", "UTC-6");
+        put("PST", "UTC-7");
+        put("BST", "UTC+0");
+    }};
 
 
-    public DiscordManager(String token, DatabaseManager databaseManager) {
+    public DiscordManager(String token, GameManager gameManager) {
         this.token = token;
-        this.databaseManager = databaseManager;
+        this.gameManager = gameManager;
+        this.databaseManager = gameManager.getDB();
         initialize();
     }
 
     public void initialize() {
         try {
             toastyBot = JDABuilder.createDefault(this.token).setActivity(Activity.playing("Coming soon ;)")).build();
+            responseHandler = new ResponseHandler(toastyBot);
 
             //EVENT LISTENERS
             toastyBot.addEventListener(new CommandHandler(this));
 
 
             //COMMANDS
-            toastyBot.upsertCommand("join", "Register to join toasty bets").queue();
+            toastyBot.upsertCommand("join", "Join Toasty Bets Today!").queue();
+            toastyBot.upsertCommand(new CommandData("register", "Register your account").
+                    addOption(OptionType.STRING, "timezone", "time zone abbreviation", true)).queue();
         }catch (Exception e) {
             LogManager.error("Failed to start discord services :", e.getMessage());
         }
@@ -40,10 +62,14 @@ public class DiscordManager {
         return databaseManager.createUser(discordId, discordName);
     }
 
-    public boolean checkIfUserIsRegistered(String discordId) {
+    public boolean checkIfUserIsCreated(String discordId) {
         if (databaseManager.getUser(discordId) == null) {
             return false;
         }
         return true;
+    }
+
+    public ResponseHandler getResponseHandler() {
+        return responseHandler;
     }
 }
