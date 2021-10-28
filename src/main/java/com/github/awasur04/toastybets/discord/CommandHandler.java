9dec59656f72d827cea48dde0d9e3a6908ec7249
awasur04 <awasur04@yahoo.com>
2021-10-28 02:08:28 -0500

    Added register command
    added functionality for register command to assist with registering/changing your time zone

--- src/main/java/com/github/awasur04/toastybets/discord/CommandHandler.java
+++ src/main/java/com/github/awasur04/toastybets/discord/CommandHandler.java
@@ -1,8 +1,6 @@
 package com.github.awasur04.toastybets.discord;
 
-import com.github.awasur04.toastybets.managers.GameManager;
-import com.github.awasur04.toastybets.managers.LogManager;
-import net.dv8tion.jda.api.entities.User;
+import com.github.awasur04.toastybets.models.User;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -13,21 +11,50 @@ public class CommandHandler extends ListenerAdapter {
         this.discordManager = dm;
     }
 
+
+    /**
+     * commands:
+     * join
+     * register
+     * generate(ND)
+     * redeem(ND)
+     * bet(ND)
+     * schedule(ND)
+     * report(ND)
+     */
+
+
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
         if (event.getName().equalsIgnoreCase("join")) {
             event.deferReply().queue();
-            User source = event.getUser();
-            if (discordManager.checkIfUserIsRegistered(source.getId())) {
-                event.getMember().getUser().openPrivateChannel().flatMap(channel -> channel.sendMessage("You are already a member")).queue();
+            net.dv8tion.jda.api.entities.User source = event.getUser();
+            if (discordManager.checkIfUserIsCreated(source.getId())) {
+                //discordManager.printWeeklySchedule(event.getUser().getId());
+                source.openPrivateChannel().flatMap(channel -> channel.sendMessage("You are not able to join once already registered, if you believe this is an error please reach out to an admin.")).queue();
             } else {
-                if (discordManager.createUser(source.getId(), source.getName())) {
-                    event.getMember().getUser().openPrivateChannel().flatMap(channel -> channel.sendMessage("Welcome thank you for joining!")).queue();
-                } else {
-                    event.getMember().getUser().openPrivateChannel().flatMap(channel -> channel.sendMessage("Please try again later")).queue();
-                }
+                discordManager.createUser(source.getId(), source.getName());
             }
             event.getHook().sendMessage("Please check your dm's").queue();
+        }else if (event.getName().equalsIgnoreCase("register")) {
+            event.deferReply().queue();
+
+            net.dv8tion.jda.api.entities.User source = event.getUser();
+            String updateTimeZone = event.getOption("timezone").getAsString().toUpperCase();
+
+            if (!discordManager.checkIfUserIsCreated(source.getId())) {
+                discordManager.createUser(source.getId(), source.getName());
+            }
+
+            User newUser = discordManager.getUser(source.getId());
+
+            if (discordManager.updateTimeZone(newUser, updateTimeZone)) {
+                discordManager.displayHelp(newUser.getDiscordId().toString());
+                discordManager.printWeeklySchedule(newUser.getDiscordId().toString());
+                event.getHook().sendMessage("Success").queue();
+            } else {
+                event.getHook().sendMessage("Invalid Time zone, please try again").queue();
+            }
         }
     }
 }
