fcf8bce5fb94a6b63f7eb5dae8ae37252f5d8642
awasur04 <awasur04@yahoo.com>
2021-11-16 00:50:42 -0600

    Craeted new DiscordService class
    DiscordService class handles all discord bot actions

---
+++ src/main/java/com/github/awasur04/ToastyBets/discord/DiscordService.java
@@ -0,0 +1,52 @@
+package com.github.awasur04.ToastyBets.discord;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PreDestroy;
+import javax.security.auth.login.LoginException;
+
+@Service
+public class DiscordService {
+
+    private JDA jda;
+
+    @Value("${discord.token}")
+    private String discordToken;
+
+    public void startBot() throws LoginException, InterruptedException {
+        this.jda = JDABuilder.createDefault(this.discordToken)
+                .setActivity(Activity.playing("Coming soon ;)"))
+                .addEventListeners(new CommandHandler())
+                .build();
+
+        registerCommands(
+                new CommandData("join", "Join Toasty Bets!"),
+                new CommandData("timezone", "Change your timezone").addOption(OptionType.STRING, "timezone", "Input your desired timezone", true),
+                new CommandData("bet", "Place a bet").addOption(OptionType.STRING, "team_abbreviation", "Team you want to bet on", true).addOption(OptionType.STRING, "amount", "Amount you would like to bet", true)
+        );
+    }
+
+    @PreDestroy
+    public void stopBot() {
+        this.jda.shutdown();
+    }
+
+    public void registerCommands(CommandData... commandData) {
+        for (CommandData command : commandData) {
+            this.jda.upsertCommand(command).queue();
+        }
+    }
+
+    public JDA getJda() {
+        return jda;
+    }
+}
