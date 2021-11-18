4a6bfcc91611fdd1c00efd3c2ad2c1a24090d942
awasur04 <awasur04@yahoo.com>
2021-11-18 10:55:12 -0600

    Added log messages for program start and stop

--- src/main/java/com/github/awasur04/ToastyBets/events/EventsHandler.java
+++ src/main/java/com/github/awasur04/ToastyBets/events/EventsHandler.java
@@ -1,15 +1,14 @@
 package com.github.awasur04.ToastyBets.events;
 
+
+import com.github.awasur04.ToastyBets.discord.CommandHandler;
 import com.github.awasur04.ToastyBets.discord.DiscordService;
 import com.github.awasur04.ToastyBets.update.UpdateGames;
 import com.github.awasur04.ToastyBets.utilities.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import javax.annotation.PreDestroy;
 
@@ -23,17 +22,19 @@ public class EventsHandler {
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationStart() {
         try {
+            LogManager.log("Program Starting");
             discordService.startBot();
             updateGames.updateSchedule();
         }catch (Exception e) {
-            LogManager.error("Main Program: ", e.getStackTrace().toString());
+            LogManager.error("Main Program: ", e.getMessage());
         }
     }
 
     @PreDestroy
     public void onApplicationClose() {
-        LogManager.log("Shutting Down");
+        LogManager.log("Program Shutting Down");
         discordService.stopBot();
         LogManager.closeLogs();
+        LogManager.log("Shutdown Complete\n");
     }
 }
