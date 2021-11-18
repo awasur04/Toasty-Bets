9fec4d592ddc037793123dfe3e31ebcc4ed6a0eb
awasur04 <awasur04@yahoo.com>
2021-11-18 10:53:51 -0600

    Moved database queries to DatabaseService class

--- src/main/java/com/github/awasur04/ToastyBets/database/DatabaseService.java
+++ src/main/java/com/github/awasur04/ToastyBets/database/DatabaseService.java
@@ -1,18 +1,26 @@
 package com.github.awasur04.ToastyBets.database;
 
-import com.github.awasur04.ToastyBets.exceptions.UserNotFoundException;
 import com.github.awasur04.ToastyBets.models.User;
 import com.github.awasur04.ToastyBets.models.enums.PermissionLevel;
 import com.github.awasur04.ToastyBets.repository.UserRepository;
 import com.github.awasur04.ToastyBets.utilities.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@Controller
-public class DatabaseController {
+import java.util.NoSuchElementException;
 
+@Component
+public class DatabaseService {
     @Autowired
     private UserRepository userRepository;
+
+    @Value("#{new Integer('${user.default.toastycoin.starting}')}")
+    private int startingBalance;
+    @Value("#{new Integer('${user.default.toastycoin.pay}')}")
+    private int weeklyPay;
+    @Value("${user.default.timezone}")
+    private String startingTimezone;
 
     public User addNewUser(String discordId, String discordName) {
         try {
@@ -20,12 +28,12 @@ public class DatabaseController {
             newUser.setDiscordId(discordId);
             newUser.setDiscordName(discordName);
             newUser.setPermissionLevel(PermissionLevel.NORMAL);
-            newUser.setTimeZone("UTC+0");
-            newUser.setToastyCoins(1000);
+            newUser.setTimeZone(startingTimezone);
+            newUser.setToastyCoins(startingBalance);
             userRepository.save(newUser);
             return newUser;
         }catch (Exception e) {
-            LogManager.error("Failed to create new user", e.getStackTrace().toString());
+            LogManager.error("Failed to create new user", e.getMessage());
             return null;
         }
     }
@@ -33,14 +41,11 @@ public class DatabaseController {
     public User findUser(String discordId) {
         try {
             User currentUser = userRepository.findById(discordId).get();
-            if (currentUser == null) {
-                throw new UserNotFoundException("ID: " + discordId);
-            }
             return currentUser;
-        } catch (UserNotFoundException u) {
+        } catch (NoSuchElementException ne) {
             return null;
         } catch (Exception e) {
-            LogManager.error("Could not retrieve user from database: " + discordId, e.getStackTrace().toString());
+            LogManager.error("Could not retrieve user from database: " + discordId, e.getMessage());
             return null;
         }
     }
@@ -50,7 +55,7 @@ public class DatabaseController {
             userRepository.save(user);
             return true;
         }catch (Exception e) {
-            LogManager.error("Failed to update user", e.getStackTrace().toString());
+            LogManager.error("Failed to update user", e.getMessage());
             return false;
         }
     }
