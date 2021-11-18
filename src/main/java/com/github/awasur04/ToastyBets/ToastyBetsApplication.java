eac5499b3d76d31738f741249a4bca04b5fcd295
awasur04 <awasur04@yahoo.com>
2021-11-18 10:57:14 -0600

    Removed unused imports

--- src/main/java/com/github/awasur04/ToastyBets/ToastyBetsApplication.java
+++ src/main/java/com/github/awasur04/ToastyBets/ToastyBetsApplication.java
@@ -1,7 +1,5 @@
 package com.github.awasur04.ToastyBets;
 
-import com.github.awasur04.ToastyBets.discord.DiscordService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
