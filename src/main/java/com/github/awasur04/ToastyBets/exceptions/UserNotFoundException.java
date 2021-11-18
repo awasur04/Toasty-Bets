e8627baf62ae7d4df8a263fb6d3e6c80c99137bb
awasur04 <awasur04@yahoo.com>
2021-11-18 10:55:54 -0600

    Removed unused exception

--- src/main/java/com/github/awasur04/ToastyBets/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.awasur04.ToastyBets.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
