dcb0487c194e888ecbc59615e45af99326960220
awasur04 <awasur04@yahoo.com>
2021-11-19 15:36:16 -0600

    Removed all old project files
    Removed old project files which are no longer needed

--- OLD/src/main/java/com/github/awasur04/toastybets/exceptions/LowBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.github.awasur04.toastybets.exceptions;
-
-public class LowBalanceException extends RuntimeException{
-    public LowBalanceException(String message) {
-        super(message);
-    }
-}
--- OLD/src/main/java/com/github/awasur04/toastybets/exceptions/TeamNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.awasur04.toastybets.exceptions;
-
-public class TeamNotFoundException extends RuntimeException{
-    public TeamNotFoundException(String message) {
-        super(message);
-    }
-}
