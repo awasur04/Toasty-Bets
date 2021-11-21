af498235271a3bfa6343642d3393d616dcbabdd0
awasur04 <awasur04@yahoo.com>
2021-11-21 01:58:38 -0600

    Added getActiveUsers database search

--- src/main/java/com/github/awasur04/ToastyBets/repository/UserRepository.java
+++ src/main/java/com/github/awasur04/ToastyBets/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package com.github.awasur04.ToastyBets.repository;
 
 import com.github.awasur04.ToastyBets.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
+    @Query(value="SELECT * FROM users WHERE NOT permission_level = 'INACTIVE'", nativeQuery = true)
+    List<User> getActiveUsers();
 }
