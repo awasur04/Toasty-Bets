9826ebde3e5eb1f5fa777cb73446a84ec112b6bc
awasur04 <awasur04@yahoo.com>
2021-11-21 02:03:44 -0600

    Create NextMidnight.java
    will return next midnight offset

---
+++ src/main/java/com/github/awasur04/ToastyBets/events/temporaladjusters/NextMidnight.java
@@ -0,0 +1,12 @@
+package com.github.awasur04.ToastyBets.events.temporaladjusters;
+
+import java.time.temporal.ChronoField;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class NextMidnight implements TemporalAdjuster {
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        return temporal.with(ChronoField.CLOCK_HOUR_OF_DAY, 0);
+    }
+}
