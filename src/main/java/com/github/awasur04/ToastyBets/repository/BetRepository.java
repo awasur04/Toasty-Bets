d998b9cf0b40677450eda01498e6831b20a4f5ef
awasur04 <awasur04@yahoo.com>
2021-11-21 01:58:58 -0600

    Added findUserCurrentWeekBets

--- src/main/java/com/github/awasur04/ToastyBets/repository/BetRepository.java
+++ src/main/java/com/github/awasur04/ToastyBets/repository/BetRepository.java
@@ -3,12 +3,16 @@ package com.github.awasur04.ToastyBets.repository;
 import com.github.awasur04.ToastyBets.models.Bet;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface BetRepository extends CrudRepository<Bet, Integer> {
-    @Query(value = "SELECT * FROM bets WHERE bet_status = 'ACTIVE'", nativeQuery = true)
+    @Query(value = "SELECT * FROM bets WHERE bet_status = 'WON' OR bet_status = 'LOSS'", nativeQuery = true)
     List<Bet> findActiveBets();
+
+    @Query(value = "SELECT * FROM bets WHERE week_number = :weekNumber AND discord_id = :discordId", nativeQuery = true)
+    List<Bet> findUserCurrentWeekBets(@Param("weekNumber") int weekNumber, @Param("discordId") String discordId);
 }
