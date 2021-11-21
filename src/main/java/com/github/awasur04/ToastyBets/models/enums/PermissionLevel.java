45f10b11200de7ed8cb83a7891417a24f9cb47de
awasur04 <awasur04@yahoo.com>
2021-11-21 01:59:59 -0600

    Gave permission levels integer values

--- src/main/java/com/github/awasur04/ToastyBets/models/enums/PermissionLevel.java
+++ src/main/java/com/github/awasur04/ToastyBets/models/enums/PermissionLevel.java
@@ -1,7 +1,18 @@
 package com.github.awasur04.ToastyBets.models.enums;
 
 public enum PermissionLevel {
-        DEV,
-        ADMIN,
-        NORMAL
+        DEV(3),
+        ADMIN(2),
+        NORMAL(1),
+        INACTIVE(0);
+
+        private int value;
+
+        PermissionLevel(int value) {
+                this.value = value;
+        }
+
+        public boolean isGreaterThan(PermissionLevel requiredLevel) {
+                return this.value >= requiredLevel.value;
+        }
 }
