70d3cbcb7e54643344b3a98fe9f6c2c63a465477
awasur04 <awasur04@yahoo.com>
2021-11-21 01:59:41 -0600

    UpdateFrequency Enums created

---
+++ src/main/java/com/github/awasur04/ToastyBets/models/enums/UpdateFrequency.java
@@ -0,0 +1,10 @@
+package com.github.awasur04.ToastyBets.models.enums;
+
+public enum UpdateFrequency {
+    WEEKLY,
+    ODDS,
+    THURSDAYSCORE,
+    SUNDAYSCORE,
+    MONDAYSCORE,
+    NIGHTLYRESET
+}
