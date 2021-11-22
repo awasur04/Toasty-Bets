c08bc8cb3815812f41325be276158794620de1b7
awasur04 <awasur04@yahoo.com>
2021-11-21 23:04:13 -0600

    Fixed GetActiveBets query

--- src/main/java/com/github/awasur04/ToastyBets/repository/BetRepository.java
+++ src/main/java/com/github/awasur04/ToastyBets/repository/BetRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Repository
 public interface BetRepository extends CrudRepository<Bet, Integer> {
-    @Query(value = "SELECT * FROM bets WHERE bet_status = 'WON' OR bet_status = 'LOSS'", nativeQuery = true)
+    @Query(value = "SELECT * FROM bets WHERE bet_status = 'ACTIVE'", nativeQuery = true)
     List<Bet> findActiveBets();
 
     @Query(value = "SELECT * FROM bets WHERE week_number = :weekNumber AND discord_id = :discordId", nativeQuery = true)
