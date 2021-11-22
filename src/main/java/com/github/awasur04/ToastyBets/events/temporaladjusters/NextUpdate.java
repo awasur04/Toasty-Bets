15f6edb832e8e1b9ea095306ed46b262b061d261
awasur04 <awasur04@yahoo.com>
2021-11-21 23:06:06 -0600

    Updated NextUpdate to return 7:00

--- src/main/java/com/github/awasur04/ToastyBets/events/temporaladjusters/NextUpdate.java
+++ src/main/java/com/github/awasur04/ToastyBets/events/temporaladjusters/NextUpdate.java
@@ -1,12 +1,13 @@
 package com.github.awasur04.ToastyBets.events.temporaladjusters;
 
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 public class NextUpdate implements TemporalAdjuster {
     @Override
     public Temporal adjustInto(Temporal temporal) {
-        return temporal.with(ChronoField.CLOCK_HOUR_OF_DAY, 7);
+        return temporal.with(ChronoField.CLOCK_HOUR_OF_DAY, 7).with(ChronoField.MINUTE_OF_HOUR, 00);
     }
 }
